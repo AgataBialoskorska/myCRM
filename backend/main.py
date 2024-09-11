fffc42c00270d3dd44cbcf1cc07003a787f2ddff
AgataBialoskorska <agatamirk@gmail.com>
2024-09-11 14:38:26 +0200

    rebuild whole logic

--- backend/main.py
+++ backend/main.py
@@ -19,12 +19,44 @@ def index():
     except Exception as e:
         return f"Flask server connection error: {e}"
     
+def select():
+        return text("SELECT * FROM sql_inventory.products;")
+def update():
+        return text("UPDATE sql_inventory.products SET ProductName = 'Butter 300g' WHERE ProductName = 'Butter 100g';")
+def delete():
+        return text("DELETE FROM sql_inventory.products WHERE ProductName = 'Peanuts 500g';")
+def insert():
+        return text("INSERT INTO sql_inventory.products (ProductName, Quantity, PurchasePrice, VAT, Margin ) VALUES ('Peanuts 500g',100, 12.65,  '5%', 0.12);")
 
-@app.route('/api/data')
-def get_data():
+    
+@app.route('/api/products')
+def get_products():
     try:
         connection = engine.connect()
-        query = text("SELECT * FROM test.products LIMIT 3")
+        # query = insert()
+        querySelect = select()
+        # result = connection.execute(query)
+        result = connection.execute(querySelect)
+
+        columns = list(result.keys())
+        rows = [list(row) for row in result]
+
+        connection.commit()
+        connection.close()
+
+        response = {'columns': columns, 'data': rows}
+
+        return jsonify(response)
+    
+    except Exception as e:
+        return jsonify({'API handling error': str(e)}), 500
+    
+    
+@app.route('/api/customers')
+def get_customers():
+    try:
+        connection = engine.connect()
+        query = text("SELECT * FROM sql_store.customers LIMIT 5")
         result = connection.execute(query)
 
         columns = list(result.keys())
@@ -33,6 +65,24 @@ def get_data():
         connection.close()
 
         response = {'columns': columns, 'data': rows}
+
+        return jsonify(response)
+    
+    except Exception as e:
+        return jsonify({'API handling error': str(e)}), 500
+    
+    
+@app.route('/api/orders')
+def get_orders():
+    try:
+        with engine.connect() as connection:
+            query = text("SELECT * FROM sql_store.orders LIMIT 5")
+            result = connection.execute(query)
+
+            columns = list(result.keys())
+            rows = [list(row) for row in result]
+
+            response = {'columns': columns, 'data': rows}
 
         return jsonify(response)
     
