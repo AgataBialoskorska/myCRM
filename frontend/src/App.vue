1197b1d2093a6f8c4ec7bda11d1d31098830e361
AgataBialoskorska <agatamirk@gmail.com>
2024-12-09 10:41:45 +0100

    improve appearance

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -78,7 +78,7 @@
 		<div>
 			<button class="dataFetchingBtn" @click="fetchData('/api/products')">GET the data - products table</button>
 		</div>
-		<div>
+		<div id="table">
 			<h2 v-if="data">Fetched Data:</h2>
 			<table v-if="data">
 				<thead>
@@ -97,50 +97,53 @@
 				</tbody>
 			</table>
 		</div>
-		<div v-if="data">
-			Choose the reaction:
+		<section id="reaction">
+			<div v-if="data">
+				<h2>Choose the reaction:</h2>
 
-			<input type="radio" id="insert" value="Insert a record" v-model="picked" />
-			<label for="insert">Insert a record</label>
+				<input type="radio" id="insert" value="Insert a record" v-model="picked" />
+				<label for="insert">Insert a record</label>
 
-			<input type="radio" id="delete" value="Delete a record" v-model="picked" />
-			<label for="delete">Delete a record</label>
+				<input type="radio" id="delete" value="Delete a record" v-model="picked" />
+				<label for="delete">Delete a record</label>
 
-			<input type="radio" id="update" value="Update a record" v-model="picked" />
-			<label for="update">Update a record</label>
+				<input type="radio" id="update" value="Update a record" v-model="picked" />
+				<label for="update">Update a record</label>
+			</div>
+			<div>
+				<h2 v-if="picked">Picked: {{ picked }}</h2>
 
-			<div>Picked: {{ picked }}</div>
-		</div>
-
-		<div v-if="(data && picked === 'Insert a record')">
-			<input type="text" placeholder="name" v-model="newValue.name" />
-			<input type="text" placeholder="quantity" v-model="newValue.quantity" />
-			<input type="text" placeholder="price" v-model="newValue.price" />
-			<input type="text" placeholder="vat" v-model="newValue.vat" />
-			<input type="text" placeholder="margin" v-model="newValue.margin" />
-			<button class="dataHandlingBtn" @click="insertData('/api/products', newValue)">
-				INSERT the data - products table
-			</button>
-		</div>
-		<div v-if="(data && picked === 'Delete a record')">
-			<input type="text" placeholder="enter ProductID" v-model="newValue.id" />
-			<button class="dataHandlingBtn" @click="deleteData('/api/products', newValue)">
-				DELETE the data from products table
-			</button>
-		</div>
-		<div v-if="(data && picked === 'Update a record')">
-			<label>Which record do you want to update?</label>
-			<input type="text" placeholder="id" v-model="updateValue.id" />
-			<label>Enter new data:</label>
-			<input type="text" placeholder="name" v-model="updateValue.name" />
-			<input type="text" placeholder="quantity" v-model="updateValue.quantity" />
-			<input type="text" placeholder="price" v-model="updateValue.price" />
-			<input type="text" placeholder="vat" v-model="updateValue.vat" />
-			<input type="text" placeholder="margin" v-model="updateValue.margin" />
-			<button class="dataHandlingBtn" @click="updateData('/api/products', updateValue)">
-				UPDATE the data - products table
-			</button>
-		</div>
+				<div v-if="data && picked === 'Insert a record'">
+					<input type="text" placeholder="name" v-model="newValue.name" />
+					<input type="text" placeholder="quantity" v-model="newValue.quantity" />
+					<input type="text" placeholder="price" v-model="newValue.price" />
+					<input type="text" placeholder="vat" v-model="newValue.vat" />
+					<input type="text" placeholder="margin" v-model="newValue.margin" />
+					<button class="dataHandlingBtn" @click="insertData('/api/products', newValue)">
+						INSERT the data - products table
+					</button>
+				</div>
+				<div v-if="data && picked === 'Delete a record'">
+					<input type="text" placeholder="enter ProductID" v-model="newValue.id" />
+					<button class="dataHandlingBtn" @click="deleteData('/api/products', newValue)">
+						DELETE the data from products table
+					</button>
+				</div>
+				<div v-if="data && picked === 'Update a record'">
+					<label>Which record do you want to update?</label>
+					<input type="text" placeholder="id" v-model="updateValue.id" />
+					<label>Enter new data:</label>
+					<input type="text" placeholder="name" v-model="updateValue.name" />
+					<input type="text" placeholder="quantity" v-model="updateValue.quantity" />
+					<input type="text" placeholder="price" v-model="updateValue.price" />
+					<input type="text" placeholder="vat" v-model="updateValue.vat" />
+					<input type="text" placeholder="margin" v-model="updateValue.margin" />
+					<button class="dataHandlingBtn" @click="updateData('/api/products', updateValue)">
+						UPDATE the data - products table
+					</button>
+				</div>
+			</div>
+		</section>
 	</div>
 	<!-- <router-view /> -->
 </template>
@@ -150,8 +153,10 @@
 		font-family: Avenir, Helvetica, Arial, sans-serif;
 		-webkit-font-smoothing: antialiased;
 		-moz-osx-font-smoothing: grayscale;
+		font-size: 16px;
 		text-align: center;
 		color: #2c3e50;
+		width: 100%;
 
 		nav {
 			padding: 30px;
@@ -167,31 +172,101 @@
 		}
 
 		button {
+			width: calc(100% - 10px);
 			padding: 10px;
 			margin: 5px;
-
-			&.dataFetchingBtn {
-				width: 80%;
-			}
-
-			&.tableUpdatingBtn {
-				width: 60px;
+			border-radius: 4px;
+			border: none;
+			cursor: pointer;
+			background-color: #42b983;
+			color: white;
+			&:hover {
+				background-color: #2c3e50;
 			}
 		}
 
 		table {
-			width: 100%;
-			padding: 10px;
-			margin: 5px;
+			display:inline-block;
+			width: 810px;
+			height: 400px;
+			overflow-y: auto;
+			resize: vertical;
+			border-collapse: collapse;
+			margin: 20px 0;
 
 			> thead {
 				text-transform: uppercase;
-				height: 30px;
 				background-color: cornflowerblue;
+				color: white;
+				position: sticky;
+				top: 0;
+				z-index: 2;
+				th {
+					padding: 10px;
+					border: 1px solid #ddd;
+				}
 			}
 
-			> tbody tr:nth-child(even) {
-				background-color: lightcyan;
+			> tbody {
+				tr {
+					width: 100%;
+				}
+				tr:nth-child(even) {
+					background-color: lightcyan;
+				}
+
+				tr:nth-child(odd) {
+					background-color: white;
+				}
+
+				td {
+					padding: 10px;
+					border: 1px solid #ddd;
+					text-align: center;
+				}
+			}
+		}
+
+		#reaction {
+			display: flex;
+			margin: 20px 25vw;
+			align-items: flex-start;
+			justify-content: space-evenly;
+
+			div:first-of-type {
+				display: grid;
+				grid-template-columns: 0.4fr 1fr;
+				align-items: baseline;
+				justify-items: start;
+				margin: 20px auto;
+
+				h2 {
+					grid-column: 1 / -1;
+				}
+				input {
+					grid-column: 1;
+				}
+				label {
+					grid-column: 2;
+				}
+			}
+
+			div:nth-child(2) {
+				display: flex;
+				flex-direction: column;
+				align-items: stretch;
+				width: 70%;
+				margin: auto;
+				input {
+					margin-top: 5px;
+				}
+				label {
+					margin-top: 5px;
+				}
+				button {
+					width: 100%;
+					margin: 10px 0;
+				}
 			}
 		}
 	}
